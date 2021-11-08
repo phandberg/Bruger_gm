39acf865771debd87ee1846fbce257726f2bff20
Deleren <Deleren@LAPTOP-6UC7JEB4>
2021-11-08 23:29:02 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System.Windows.Forms;
+
+namespace Bruger_gm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            map.DragButton = MouseButtons.Left;
+            map.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            double lat = Convert.ToDouble(txtlat.Text);
+            double lon = Convert.ToDouble(txtlon.Text);
+            map.Position = new GMap.NET.PointLatLng(lat, lon);
+            map.MinZoom = 1;
+            map.MaxZoom = 18;
+            map.Zoom = 17;
+            
+            PointLatLng point = new PointLatLng(lat, lon);
+            GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.red_pushpin);
+
+            GMapOverlay markers = new GMapOverlay("markers");
+
+            markers.Markers.Add(marker);
+
+            map.Overlays.Add(markers);
+        }
+
+        private void map_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
